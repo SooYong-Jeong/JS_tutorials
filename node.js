f30a513cf5a0a2c89a1399c9c089bcd024104cf2
JEONG SOOYONG <wjdtndyd7@naver.com>
2023-12-14 23:32:55 +0900

    API

--- node.js
+++ node.js
@@ -4,14 +4,24 @@ const app = express();
 app.listen(8080, function(){
     console.log('listening on 8080')
 });
+ app.get('/sound/:name', function(req, res){
+    const { name } = req.params
+    console.log(name)
+    if(name == "kakaotalk")
+    res.json({'sound':'kakaotalk'})
+    else if(name == "discord")
+    res.json({'sound':'discord'})
+    else
+    res.json({'sound':'unknown'})
+ });
 
-app.get('/discord', function(req, res){
-    res.send('discord death');
-});
+// app.get('/discord', function(req, res){
+//     res.send('discord death');
+// });
 
-app.get('/kakao', function(req, res){
-    res.send('kakao death');
-});
+// app.get('/kakao', function(req, res){
+//     res.send('kakao death');
+// });
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
